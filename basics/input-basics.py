514fb899b2f7de27209c2dad2ebdf5cb86099b52
t0talstorm <adityaishan007@gmail.com>
2024-12-21 19:15:02 +0530

    basic input

---
+++ basics/input-basics.py
@@ -0,0 +1,10 @@
+#input() = A function that allows user input
+# returned as a string
+
+name = input("Enter your name: ")
+
+age = int(input("Enter your age: "))
+
+age += 1
+
+print(f"you are {name} and you are {age} years old")
