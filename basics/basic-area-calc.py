1fc93c40077a490b5e43930b1b15d05bb96bd76a
t0talstorm <adityaishan007@gmail.com>
2024-12-21 19:14:20 +0530

    basic

---
+++ basics/basic-area-calc.py
@@ -0,0 +1,7 @@
+#Area of a rectangle
+
+length = float(input("Enter the length of the rectangle (in cm) : "))
+width = float(input("Enter the width of the rectangle (in cm) : "))
+
+area = length * width
+print(f"\nThe area of the rectangle is {area}cm²") 
